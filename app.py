c4fd3174c18d8ceece0c9ab278745999d3920d50
hyunnn24 <snow3515@naver.com>
2024-08-13 06:41:01 +0000

    ver4

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def birth():
 def age():
     st.write("Age Page")  # Placeholder content
 
-page = st.sidebar.selectbox("LET'S GET THE KING :) \n made by hyunnn \n CERT-IS PKNU \n 2024.08.13", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE"])
+page = st.sidebar.selectbox("LET'S GET THE KING :)", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE"])
+st.sidebar.text("made by hyunnn \n CERT-IS PKNU \n 2024.08.13")
 
 if page == "PHONE NUMBER":
     phone()
