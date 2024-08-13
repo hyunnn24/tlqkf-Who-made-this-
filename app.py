39c939a17553e3dcb4a1f7b93ac781db50b4b4d1
hyunnn24 <snow3515@naver.com>
2024-08-13 06:45:28 +0000

    VOLUME TAB ADDED

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ def birth():
 def age():
     st.write("Age Page")  # Placeholder content
 
+def VOLUME():
+    st.write(" volume page")
+
+
+
+
+
 page = st.sidebar.selectbox("LET'S GET THE KING :)", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE"])
 st.sidebar.text("  made by hyunnn \n  CERT-IS PKNU \n  2024.08.13")
 
@@ -25,3 +32,5 @@ elif page == "AGE":
     age()
 elif page == "MAIN":
     main()
+elif page == "VOLUME":
+    VOLUME()
