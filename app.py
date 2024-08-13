a5665cfacd729274bbad107d1631be223244ebc7
hyunnn24 <snow3515@naver.com>
2024-08-13 07:13:59 +0000

    ver3

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def VOLUME():
 def SECRET():
     st.title("Do You Want To Know The Secret? :lock:")
 
-page = st.sidebar.selectbox("LET'S GET THE KING :)", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE","VOLUME","SECRET :lock:"])
+page = st.sidebar.selectbox("LET'S GET THE KING :)", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE","VOLUME","SECRET"])
 st.sidebar.text("  made by hyunnn :kr: \n  CERT-IS PKNU \n  2024.08.13")
 
 if page == "PHONE NUMBER":
@@ -34,5 +34,5 @@ elif page == "MAIN":
     main()
 elif page == "VOLUME":
     VOLUME()
-elif page == "SECRET :lock:":
+elif page == "SECRET":
     SECRET()
