fe6fffd3eb0df2dc854b3b8ce19863baceb38f28
hyunnn24 <snow3515@naver.com>
2024-08-13 05:56:38 +0000

    pade added

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-page = st.sidebar.selectbox("GET THE KING", ["PHONE NUMBER", "BIRTH DAY", "AGE"])
+page = st.sidebar.selectbox("GET THE KING", ["MAIN","PHONE NUMBER", "BIRTH DAY", "AGE"])
+
+if page == "PHONE NUMBER":
+    phone()
+elif page == "BIRTH DAY":
+    birth()
+elif page == "AGE":
+    age()
+elif page == "MAIN":
+    main()
+
+def main():
+    st.write("Hello, World")
+def phone():
+
+def birth():
+
+def age():
