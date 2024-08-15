214e1a6214b0c7e4a3841f59ffb34308a7c01ab3
hyunnn24 <snow3515@naver.com>
2024-08-15 12:12:57 +0000

    나이범위 재수정

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def birth():
     st.title("Let's Have a Party On Your Birthday! :partying_face:") 
 
 if "age_ran" not in st.session_state:
-    st.session_state.age_ran = random.randint(17, 80)
+    st.session_state.age_ran = random.randint(17, 60)
 
 def age():
     st.title("How Old Are You? :smile:")
